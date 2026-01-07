834708033c94511f2104f8cd06014c4043f2b5f7
lsdixit <dixitleela@gmail.com>
2026-01-06 16:29:00 -0800

    Create function for solar energy calculation

---
+++ Assignments/yield.R
@@ -0,0 +1,26 @@
+#' Energy produced from a photovoltaic system
+#' 
+#' This function will compute the energy produced by a photovoltaic system (for example, solar panel systems). 
+#'
+#' @param A the solar panel area (m^2)
+#' @param H annual average solar radiation (kWh m^-2)
+#' 
+#' Two parameters are kept constant within the function. 
+#' r is the panel yield defined by manufacturer efficiency, typically ~0.2. PR is the performance ratio that accounts for site specific losses, typically ~0.75.
+#'
+#' @returns a number of type numeric representing the total energy produced by the solar panel specifications.
+#' @export
+#'
+#' @examples 
+#' yield(A = 1.5, H = 1000)
+
+
+yield <- function(A, H) {
+  # define constant parameters
+  r <- 0.2 
+  PR <- 0.75
+  
+  # define energy production equation
+  energy = A * r * H * PR
+  return(energy)
+}
